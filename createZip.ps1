aaebf01e44f0f70951faee1dd8b3993bd8c1f835
Chris Kader <chris.kader@gmail.com>
2021-10-01 14:36:08 -0500

    Updates to PowerShell Script

--- createZip.ps1
+++ createZip.ps1
@@ -1,7 +1,8 @@
+$tag = "v1.0.6"
 mkdir .\addon\LFM+
 mkdir .\addon\LFM+\locale
 Copy-Item -Path ".\*" -Include *.lua, *.xml, *.toc -Destination ".\addon\LFM+\"
 Copy-Item -Path ".\locale\*" -Include *.lua -Destination ".\addon\LFM+\locale\"
 Copy-Item .\libs addon\LFM+\libs -Recurse
-Compress-Archive .\addon\LFM+\ .\release\LFM+.zip -Force
+Compress-Archive .\addon\LFM+\ ".\release\LFM+_$tag.zip" -Force
 rm .\addon\ -r -fo
